1813faaa6b54dee0ba2705f778ebedc743387526
henriquemem <henriquemem@gmail.com>
2016-01-13 17:44:30 -0200

    at

--- farma-FX-view/src/main/java/br/com/datarey/Start.java
+++ farma-FX-view/src/main/java/br/com/datarey/Start.java
@@ -3,13 +3,17 @@ package br.com.datarey;
 
 import com.sun.javafx.css.StyleManager;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class Start extends Application {
 
@@ -19,6 +23,7 @@ public class Start extends Application {
     public void start(Stage primaryStage) throws Exception {
         Application.setUserAgentStylesheet(null);
         StyleManager.getInstance().addUserAgentStylesheet("/br/com/datarey/css/global.css");
+        Thread.setDefaultUncaughtExceptionHandler(Start::showError);
         primaryStage.initStyle(StageStyle.UNDECORATED);
         Image image = new Image(Start.class.getResourceAsStream("logo-datarey.png"));
         ImageView imageView = new ImageView(image);
@@ -33,6 +38,27 @@ public class Start extends Application {
         this.outLabel.textProperty().bindBidirectional(task.getMsg());
         
         new Thread(task).start();
+    }
+
+
+    private static void showError(Thread t, Throwable e) {
+        if (Platform.isFxApplicationThread()) {
+            Platform.runLater(() -> {
+                Alert alert = null;
+                alert = new Alert(Alert.AlertType.ERROR);
+                String msg = e.getMessage();
+                if(e.getCause() != null && e.getCause() instanceof InvocationTargetException){
+                        msg = msg + "\n" + ((InvocationTargetException)e.getCause()).getTargetException().getMessage();
+                }else {
+                    if (e.getCause() != null && e.getCause().getMessage() != null && !e.getCause().getMessage().equals(msg)) {
+                        msg = msg + "\n" + e.getCause();
+                    }
+                }
+                alert.setHeaderText(msg == null ? "null" : msg);
+                alert.show();
+                e.printStackTrace();
+            });
+        }
     }
     
    
