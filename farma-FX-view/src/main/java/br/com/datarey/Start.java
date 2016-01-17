6c99452e75dbc86bdb83104b34e103a009b32cc3
henrique <henriquemem@gmail.com>
2016-01-17 14:51:23 -0200

    at

--- farma-FX-view/src/main/java/br/com/datarey/Start.java
+++ farma-FX-view/src/main/java/br/com/datarey/Start.java
@@ -4,9 +4,7 @@ package br.com.datarey;
 import br.com.datarey.sys.util.ExceptionMessageUtil;
 import com.sun.javafx.css.StyleManager;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -14,17 +12,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import javax.validation.ConstraintViolationException;
-import java.lang.reflect.InvocationTargetException;
-
 public class Start extends Application {
 
     private Label outLabel = new Label();
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Application.setUserAgentStylesheet(null);
-        StyleManager.getInstance().addUserAgentStylesheet("/br/com/datarey/css/global.css");
         Thread.setDefaultUncaughtExceptionHandler(ExceptionMessageUtil::showError);
         primaryStage.initStyle(StageStyle.UNDECORATED);
         Image image = new Image(Start.class.getResourceAsStream("logo-datarey.png"));
